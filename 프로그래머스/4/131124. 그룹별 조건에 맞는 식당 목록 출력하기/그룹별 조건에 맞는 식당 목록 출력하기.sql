40c867855f2668d9bebe8c45d5148782c1398dfb
hyeri <135313008+yoonhyeri@users.noreply.github.com>
2024-02-02 20:35:40 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,19 +1,35 @@
-# # MEMBER_PROFILE와 REST_REVIEW 테이블에서 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요. 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고, 결과는 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요
-# SELECT MEMBER_NAME, REVIEW_TEXT, REVIEW_DATE
+SELECT *, COUNT(*),
+    DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) AS ReviewRank
+FROM REST_REVIEW
+GROUP BY MEMBER_ID
+
+
+# SELECT MEMBER_NAME, REVIEW_TEXT, 
+#        DATE_FORMAT(REVIEW_DATE,'%Y-%m-%d') AS REVIEW_DATE
 # FROM MEMBER_PROFILE JOIN REST_REVIEW USING (MEMBER_ID)
+# WHERE MEMBER_ID IN (SELECT MEMBER_ID
+#                     FROM REST_REVIEW
+#                     GROUP BY MEMBER_ID
+#                     HAVING COUNT(*) = (SELECT MAX(CNT) AS MAX_CNT
+#                                        FROM(SELECT *, COUNT(*) AS CNT
+#                                             FROM REST_REVIEW
+#                                             GROUP BY MEMBER_ID
+#                                             ORDER BY COUNT(*) DESC
+#                                             ) INLINE
+#                                       )
+#                    )
+# ORDER BY REVIEW_DATE, REVIEW_TEXT
 
 
-# WHERE MEMBER_ID IN (
-#     SELECT MEMBER_ID
-#     FROM REST_REVIEW
-#     GROUP BY MEMBER_ID
-#     ORDER BY COUNT(*) DESC
-# )
-
-SELECT *
-FROM(
-    SELECT *, COUNT(*) AS CNT
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY COUNT(*) DESC
-    ) INLINE
+/*
+MEMBER_NAME	REVIEW_TEXT	REVIEW_DATE
+정소율 쫄깃하면서도··· 2022-01-14
+김민재 안에 찰밥이···  2022-02-18
+김민재 사람들이 왜···  2022-02-21
+정소율	순대국은 얼··· 2022-03-11
+김민재 삼겹보다 목··· 2022-03-15
+정소율 쟁반국수와··· 2022-03-22
+김서준 끝도 없이 주··· 2022-04-11
+김서준 우리가 흔히··· 2022-04-16
+김서준	바삭바삭해요··· 2022-05-23
+*/
