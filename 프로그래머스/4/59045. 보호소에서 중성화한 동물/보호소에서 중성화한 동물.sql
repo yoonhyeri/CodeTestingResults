eb5f7e86597b52908772366c7133a5b5e510b3e7
hyeri <135313008+yoonhyeri@users.noreply.github.com>
2024-02-04 00:28:17 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,2 +1,12 @@
--- 코드를 입력하세요
-SELECT
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O USING(ANIMAL_ID)
+WHERE I.SEX_UPON_INTAKE LIKE 'Intact%' 
+    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%' 
+         OR  O.SEX_UPON_OUTCOME LIKE 'Neutered%')      
+ORDER BY I.ANIMAL_ID                                                               
+
+/*
+ANIMAL_ID	ANIMAL_TYPE	NAME
+A382192	Dog	Maxwell 2
+A410330	Dog	Chewy
+*/
